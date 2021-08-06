3912c90af8d81368c025e979b3fbbec539966770
Faramarz Bodaghi <Faramarz.Bodaghi@dnv.com>
2021-08-06 10:49:40 +0200

    #3 fixed

--- SharedViews/Pages/Error.cshtml.cs
+++ SharedViews/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace IAmFara.Web.Pages
+namespace IAmFara.Pages.SharedViews
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
