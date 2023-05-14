f03db4685b5da050639323c2da7dfa1808464d22
Faramarz Bodaghi <me@iamfara.com>
2023-05-14 21:20:22 +0200

    #39: Adding frontend project (#42)

---
+++ src/Frontend/iamfara/src/app/app.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return <StyledApp>Hello world</StyledApp>;
+}
+
+export default App;
