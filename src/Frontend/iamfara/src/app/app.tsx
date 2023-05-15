9a2d6ce2732a8f7ea4eec6dbe1f4498f3e9b3533
Faramarz Bodaghi <me@iamfara.com>
2023-05-15 10:09:13 +0200

    Added a react component (#43)

--- src/Frontend/iamfara/src/app/app.tsx
+++ src/Frontend/iamfara/src/app/app.tsx
@@ -1,11 +1,16 @@
 import styled from '@emotion/styled';
+import TestComponent from 'src/components/test-component/TestComponent';
 
 const StyledApp = styled.div`
   // Your style here
 `;
 
 export function App() {
-  return <StyledApp>Hello world</StyledApp>;
+  return (
+    <StyledApp>
+      <TestComponent />
+    </StyledApp>
+  );
 }
 
 export default App;
---
+++ src/Frontend/iamfara/src/components/test-component/TestComponent.tsx
@@ -0,0 +1,3 @@
+export default function TestComponent() {
+  return <div>This is my first react component</div>;
+}
