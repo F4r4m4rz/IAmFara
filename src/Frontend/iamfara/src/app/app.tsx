1944b5aa01fffed67b18c73db9acabe84332a87b
Faramarz Bodaghi <faramarz.bodaghi@dnv.com>
2023-06-19 20:05:12 +0200

    Navbar links

--- src/Frontend/iamfara/src/app/app.tsx
+++ src/Frontend/iamfara/src/app/app.tsx
@@ -1,19 +1,14 @@
-import styled from '@emotion/styled';
 import { Route, Routes } from 'react-router-dom';
 import { LayoutShell } from 'src/components/layout-shell/LayoutShell';
-import TestComponent from 'src/components/test-component/TestComponent';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
 
 export function App() {
   return (
     <Routes>
       <Route element={<LayoutShell />}>
         <Route path="" element={<>Home page</>} />
-        <Route path="Blog" element={<>Blog page</>} />
-        <Route path="Test" element={<TestComponent />} />
+        <Route path="about" element={<>About me page</>} />
+        <Route path="blog" element={<>Blog page</>} />
+        <Route path="contact" element={<>Contact me page</>} />
       </Route>
     </Routes>
   );
